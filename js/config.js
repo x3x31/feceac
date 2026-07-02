ffefe54cb346231a913052eb4c5cbd6a9ea2a07b
Michel Alves <x3x31@hotmail.com>
2026-07-02 03:37:21 -0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,4 +1,4 @@
-export const SUPABASE_URL = "https://ngcawgaupbjdhlayqfmx.supabase.co/rest/v1/";
+export const SUPABASE_URL = "https://ngcawgaupbjdhlayqfmx.supabase.co";
 export const SUPABASE_ANON_KEY =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5nY2F3Z2F1cGJqZGhsYXlxZm14Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODI5NTQ4NjQsImV4cCI6MjA5ODUzMDg2NH0.gHSmRJ9Cn7up5dACN9X1CX_5xWpe7pe9piygFI7TY0w";
 
